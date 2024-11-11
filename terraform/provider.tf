5be24edecc72a3d233eb18bffa5b9c0908b2b300
rajakandukuru <raja.kandukuru@gmail.com>
2024-11-11 13:55:01 +0000

    initial commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,12 +6,14 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "******"
-    key = "******"
-    region = "ap-south-1"
+    bucket = "intso-education"
+    key = "backend"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
+  access_key = "AKIAZI2LCYQIIO6X2JT3"
+  secret_key = "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"
 }
